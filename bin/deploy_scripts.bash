287e6d2ae4af9b8b51046c6cdfcb6edb91a4e2a6
Alex Rock Ancelet <pierstoval@gmail.com>
2021-11-10 16:41:04 +0100

    Update deploy script

--- bin/deploy_scripts.bash
+++ bin/deploy_scripts.bash
@@ -30,6 +30,6 @@ php bin/console doctrine:migrations:migrate --no-interaction
 
 php bin/console doctrine:schema:validate || echo 'Doctrine schema not valid, please make sure it is correct.'
 
-npm install
+yarn install
 
-npm run build
+yarn run build
